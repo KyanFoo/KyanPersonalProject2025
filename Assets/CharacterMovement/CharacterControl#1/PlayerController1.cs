3f4c2b16593fa513efcd16604ceec6fc04bcb685
KyanFoo <kyanfoozk@gmail.com>
2025-05-11 19:02:25 +0800

    Updated "CharacterMovement_Scene"
    Rename Character Controllers Scripts to be the same for neat purposes.
    Rearrange GameObjects in Scene.

--- Assets/CharacterMovement/CharacterControl#1/PlayerController1.cs
+++ Assets/CharacterMovement/CharacterControl#1/PlayerController1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharMovement : MonoBehaviour
+public class PlayerController1 : MonoBehaviour
 {
     public CharacterController controller;
 
--- Assets/CharacterMovement/CharacterControl#3/PlayerController#3.cs
+++ Assets/CharacterMovement/CharacterControl#3/PlayerController#3.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController3 : MonoBehaviour
 {
     [SerializeField]
     private float playerSpeed = 2.0f;
