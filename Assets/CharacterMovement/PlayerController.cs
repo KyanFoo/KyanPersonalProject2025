306c0b0e7f57d872a8eab673c36bd830dd63a4e5
KyanFoo <kyanfoozk@gmail.com>
2025-05-17 17:02:36 +0800

    Updated "PersonalCharacterControl" (2/4)
    Created "Ground Check", "Player Movement", "Gravity", "Sprint", "MovementState".

--- Assets/CharacterMovement/PlayerController.cs
+++ Assets/CharacterMovement/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [Header("Reference")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private Transform player;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private MovementState state;
 
     [Header("Camera Movement Setting")]
     [SerializeField] private float sensX;
@@ -15,22 +17,117 @@ public class PlayerController : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    [Header("Movement Setting")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float sprintSpeedMultiplier = 2f;
+    [SerializeField] private float sprintTransitSpeed = 5f;
+    [SerializeField] private float groundDrag = 5f;
+    [SerializeField] private float targetGravity = -30f;
+    float horizontalInput;
+    float verticalInput;
+
+    public float currentSpeedMultiplier;
+    public float currentSpeed;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Ground Check")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private bool grounded;
+
+    private enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
     // Start is called before the first frame update
     void Start()
     {
         //Lock and remove Cursor from screen.
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // Assign Rigidbody & Freeze Rotation.
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Ground Check/
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        // Handle Drag (Have some friction so that the character is not moving too fast).
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0f;
+        }
+
         Movement();
+    }
+    private void FixedUpdate()
+    {
+        // Calculate extra gravity needed
+        float extraGravity = targetGravity - Physics.gravity.y;
+        rb.AddForce(Vector3.up * extraGravity, ForceMode.Acceleration);
+
+        GroundMovement();
     }
     private void Movement()
     {
         CameraMovement();
+    }
+
+    private void GroundMovement()
+    {
+        // Get keyboard Input.
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Movement direction relative to camera
+        Vector3 move = new Vector3(horizontalInput, 0, verticalInput);
+        move = virtualCamera.transform.TransformDirection(move);
+
+        // Change state of Player.
+        StateHandler();
+
+        // Smooth transition between walk and sprint speed
+        currentSpeed = Mathf.Lerp(currentSpeed, moveSpeed * currentSpeedMultiplier, sprintTransitSpeed * Time.deltaTime);
+        move *= currentSpeed;
+
+        // Apply force to Rigidbody
+        rb.AddForce(move * moveSpeed * 5f, ForceMode.Force);
+    }
+    public void StateHandler()
+    {
+        // Mode - Sprinting
+        if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            currentSpeedMultiplier = sprintSpeedMultiplier;
+        }
+
+        // Mode - Walking
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            currentSpeedMultiplier = 1f;
+        }
+
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+        }
     }
 
     private void CameraMovement()
