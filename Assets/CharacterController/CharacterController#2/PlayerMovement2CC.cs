8a0ee37f1947b5ab1457bfed130832ed188f0ad7
KyanFoo <kyanfoozk@gmail.com>
2025-05-31 20:06:19 +0800

    Updated [CharacterController#2RB] & Create [CharacterController#2CC] 2/2
    Update on [CharacterController#2], renaming them for RB(Rigidbody) or CC(CharacterController) so that there is no mix ups.

---
+++ Assets/CharacterController/CharacterController#2/PlayerMovement2CC.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2CC : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Vector3 playerMovementInput;
+    private Vector3 playerMouseInput;
+    private float xRot;
+
+    [SerializeField] private Transform playerCam;
+    [SerializeField] private CharacterController controller;
+    [Space]
+    [SerializeField] private float speed;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity = -9.81f;
+
+    private void Update()
+    {
+        playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        MovePlayer();
+        MovePlayerCam();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 MoveVector = transform.TransformDirection(playerMovementInput);
+
+        if(controller.isGrounded)
+        {
+            velocity.y = -1f;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = jumpForce;
+            }
+        }
+        else
+        {
+            velocity.y -= gravity * -2f * Time.deltaTime;
+        }
+
+        controller.Move(MoveVector * speed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+    private void MovePlayerCam()
+    {
+        xRot -= playerMouseInput.y * sensitivity;
+
+        transform.Rotate(0f, playerMouseInput.x * sensitivity, 0f);
+        playerCam.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+}
--- Assets/CharacterController/CharacterController#2/PlayerMovement2RB.cs
+++ Assets/CharacterController/CharacterController#2/PlayerMovement2RB.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement2 : MonoBehaviour
+public class PlayerMovement2RB : MonoBehaviour
 {
     private Vector3 playerMovementInput;
     private Vector2 playerMouseInput;
