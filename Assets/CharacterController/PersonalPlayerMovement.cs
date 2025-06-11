21209c429b64a42e3c5faf8bb1f523e975d95184
KyanFoo <kyanfoozk@gmail.com>
2025-06-11 22:35:33 +0800

    Create [PersonalCharacterController] (3/8)
    Reference from:
    [CharacterController#1], StateHandler() and SpeedControl() Functions.
    
    [CharacterController#6], for MovePlayer() and FrictionForces() Functions.

--- Assets/CharacterController/PersonalPlayerMovement.cs
+++ Assets/CharacterController/PersonalPlayerMovement.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class PersonalPlayerMovement : MonoBehaviour
 {
+    public float minVelocity = 0.1f;
+    public float velocityMagnitude;
+    private const float GROUND_CHECK_SPHERE_OFFSET = 0.05f;
+
     [Header("Reference")]
+    [SerializeField] private MovementState state;
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private Transform playerBody;
     [SerializeField] private Rigidbody playerRigidbody;
     [SerializeField] private CapsuleCollider playerCollider;
+    [SerializeField] private Transform orientation;
+
 
     [Header("Input Settings")]
     [SerializeField] private float sensX;
@@ -23,23 +31,127 @@ public class PersonalPlayerMovement : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    float verticalInput;
+    float horizontalInput;
+
+    Vector3 finalDir;
+
+    [Header("Movement Settings")]
+    float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sprintSpeed;
+
+    [Header("Drag")]
+    [SerializeField] float groundDrag = 6f;
+    [SerializeField] float airDrag = 2f;
+
     [Header("GroundCheck Settings")]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float groundCheckDistance = 0.05f;
     [SerializeField] public bool isGrounded;
 
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
     [Header("DebugDraw Settings")]
     public bool debug;
     public float drawSphereSize;
 
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
+
     private void Update()
     {
         InputManagement();
+        SpeedControl();
+        Statehandler();
+
         CameraMovement();
     }
     private void FixedUpdate()
     {
         isGrounded = IsGrounded();
+
+        GroundMovement();
+        ControlDrag();
+    }
+
+    private void Statehandler()
+    {
+        // Mode - Sprinting
+        if (isGrounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        // Mode - Walking
+        else if (isGrounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+        }
+
+    }
+
+    private void GroundMovement()
+    {
+        Vector3 dir = Vector3.zero;
+        dir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        finalDir = dir.normalized;
+        if (isGrounded)
+        {
+            playerRigidbody.AddForce(finalDir * moveSpeed * 10f, ForceMode.Force);
+        }
+    }
+
+    private void ControlDrag()
+    {
+        if (isGrounded)
+        {
+            playerRigidbody.drag = groundDrag;
+        }
+        else
+        {
+            playerRigidbody.drag = airDrag;
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            playerRigidbody.velocity = new Vector3(limitedVel.x, playerRigidbody.velocity.y, limitedVel.z);
+        }
+
+        velocityMagnitude = flatVel.magnitude; // optional: for debugging or UI
+
+        if (finalDir == Vector3.zero && isGrounded)
+        {
+            if (playerRigidbody.velocity.magnitude < minVelocity)
+            {
+                playerRigidbody.velocity = Vector3.zero;
+            }
+            else
+            {
+                // Apply friction opposite to velocity
+                Vector3 frictionForce = -playerRigidbody.velocity.normalized * groundDrag;
+                playerRigidbody.AddForce(frictionForce);
+            }
+        }
     }
 
     private void CameraMovement()
@@ -75,9 +187,8 @@ public class PersonalPlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        //bool isGrounded = Physics.SphereCast(FeetPosition() + upOffset, playerCollider.radius * transform.localScale.x, -1 * transform.up, out RaycastHit info, groundCheckDistance + GROUND_CHECK_SPHERE_OFFSET, groundMask);
-        bool isGrounded = Physics.SphereCast(FeetPosition(), playerCollider.radius * transform.localScale.x, -1 * transform.up, out RaycastHit info, groundCheckDistance, groundMask);
-
+        Vector3 upOffset = transform.up * GROUND_CHECK_SPHERE_OFFSET;
+        bool isGrounded = Physics.SphereCast(FeetPosition() + upOffset, playerCollider.radius * transform.localScale.x, -1 * transform.up, out RaycastHit info, groundCheckDistance + GROUND_CHECK_SPHERE_OFFSET, groundMask);
 
         return isGrounded;
     }
@@ -104,8 +215,12 @@ public class PersonalPlayerMovement : MonoBehaviour
 
     private void InputManagement()
     {
+        // Gather inputs from the WASD keys for movement.
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
         //Gather input from the mouse for camera movement.
-        mouseX = Input.GetAxis("Mouse X");
-        mouseY = Input.GetAxis("Mouse Y");
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
     }
 }
