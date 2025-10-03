5bf8b15ff4c2df9011ef3f5a3327492569d24329
KyanFoo <kyanfoozk@gmail.com>
2025-10-03 13:27:23 +0800

    Create [PersonalCharacterController] #2
    Reference:
    OnDrawGizmos()
    [CharacterController4]. Edited

--- Assets/CharacterController/PersonalCharacterController/PlayerMovement.cs
+++ Assets/CharacterController/PersonalCharacterController/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Cinemachine;
 using UnityEngine;
 
@@ -31,6 +32,22 @@ namespace KyanPersonalProject2025.PersonalCharacterController
         [SerializeField] private float groundCheckDistance = 0.05f;
         private const float GROUND_CHECK_SPHERE_OFFSET = 0.05f;
         public bool isGrounded;
+
+        [Header("DebugDraw Settings")]
+        public bool debug = false;
+        [System.Flags] public enum DebugTesting
+        {
+            None = 0,
+            Debug1_FeetPosition = 1 << 0,
+            Debug2_CastIsGrounded = 1 << 1,
+            Debug3_IsGrounded = 1 << 2,
+            Debug4_LerpMotion = 1 << 3,
+            Debug5 = 1 << 4,
+            Debug6 = 1 << 5,
+            Debug7 = 1 << 6,
+        }
+        public DebugTesting debugTypes;
+        [Range(0f, 1f)][SerializeField] float debugCastProgress = 0f;
 
         public void Start()
         {
@@ -87,6 +104,50 @@ namespace KyanPersonalProject2025.PersonalCharacterController
 
             mouseX = Input.GetAxisRaw("Mouse X");
             mouseY = Input.GetAxisRaw("Mouse Y");
+        }
+
+        private void OnDrawGizmos() // --- Debug gizmos for visualizing ground check ---
+        {
+            if (debug)  // Only run if debug mode is enabled.
+            {
+                if (debugTypes.HasFlag(DebugTesting.Debug1_FeetPosition)) // Show the FeetPosition as a wire sphere.
+                {
+                    //Draw a WireSphere to show the FeetPosition point.
+                    Gizmos.DrawWireSphere(FeetPosition(), playerCollider.radius * transform.localScale.x);
+                }
+
+                if (debugTypes.HasFlag(DebugTesting.Debug2_CastIsGrounded)) // Show the SphereCast origin (slightly above feet).
+                {
+                    //Draw a WireSphere to show the IsGrounded point slightly above the FeetPositon to avoid clipping. ###
+                    Vector3 checkOrigin = FeetPosition() + transform.up * GROUND_CHECK_SPHERE_OFFSET;
+
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireSphere(checkOrigin, playerCollider.radius * transform.localScale.x);
+                }
+
+                if (debugTypes.HasFlag(DebugTesting.Debug3_IsGrounded)) // Show the expected grounded check endpoint.
+                {
+                    // Draw a WireSphere to visualize a FeetPosition Collidier for Player Prefab for Ground & Slope Check.
+                    Vector3 feetOffset = -1 * groundCheckDistance * transform.up;
+
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawWireSphere(FeetPosition() + feetOffset, playerCollider.radius * transform.localScale.x);
+                }
+
+                if (debugTypes.HasFlag(DebugTesting.Debug4_LerpMotion)) // Visualize the entire SphereCast motion using interpolation.
+                {
+                    // SphereCast start point (above feet to avoid clipping)
+                    Vector3 checkOrigin = FeetPosition() + transform.up * GROUND_CHECK_SPHERE_OFFSET;
+                    Vector3 feetOffset = -1 * groundCheckDistance * transform.up;
+
+                    // ----- Motion simulation -----
+                    // debugCastProgress = 0 > start, 1 > end
+                    Vector3 movingSpherePos = Vector3.Lerp(checkOrigin, FeetPosition() + feetOffset, debugCastProgress);
+                    Gizmos.color = Color.cyan; // moving sphere color
+                    Gizmos.DrawWireSphere(movingSpherePos, playerCollider.radius * transform.localScale.x);
+                }
+
+            }
         }
     }
 }
