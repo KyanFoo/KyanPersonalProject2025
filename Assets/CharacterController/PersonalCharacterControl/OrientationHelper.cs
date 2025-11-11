0f8761b058a029fa565ce5c98c11704c588ea222
KyanFoo <kyanfoozk@gmail.com>
2025-11-11 18:04:17 +0800

    Update [PersonalCharacterControl] #6
    Added [OrientationHelper]_Script into my PersonalCharacterConrol Prefab.
       - Now it can move around.

---
+++ Assets/CharacterController/PersonalCharacterControl/OrientationHelper.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientationHelper : MonoBehaviour
+{
+    public Transform playerMesh;
+    public LayerMask groundMask;
+    public float rayLength = 5f;
+
+    public float rotSpeed = 2f;
+    public float rot;
+
+    private void FixedUpdate()
+    {
+        bool hit = Physics.Raycast(transform.position, Vector3.down, out RaycastHit info, rayLength, groundMask);
+        if (hit)
+        {
+            transform.up = info.normal;
+        }
+
+        transform.Rotate(Vector3.up, playerMesh.transform.eulerAngles.y - transform.eulerAngles.y, Space.Self);
+
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 5f, Color.red);
+    }
+}
